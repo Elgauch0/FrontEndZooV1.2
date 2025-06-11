8b8a8eaf1dfd6c906dba0ea0c88971d07ccbe664
Anass El kaouri <elkaourianass@gmail.com>
2025-06-11 19:26:28 +0200

    editing URL

--- src/functions.js
+++ src/functions.js
@@ -2,8 +2,8 @@
 const  token =sessionStorage.getItem('token'); 
 //const API_URL = import.meta.env.VITE_API_URL;
 //const API_SOURCE = import.meta.env.VITE_API_SOURCE
-const API_URL = 'http://51.47.198.51/api/';
-const API_SOURCE = 'http://51.47.198.51/';
+const API_URL = 'http://52.47.198.51/api/';
+const API_SOURCE = 'http://52.47.198.51/';
 export { API_SOURCE };
 
 
